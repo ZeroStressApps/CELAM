fd1e4bb47d618627e2e5db12248ecd9e206240c5
Lau <l.somacarrera@gmail.com>
2026-09-11 11:31:40 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="celam-v9-auth-1";
+const CACHE="celam-v9-auth-2";
 const FILES=["./","./index.html","./style.css","./data.js","./app.js","./auth.js","./manifest.webmanifest","./assets/celam-logo.png"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));self.skipWaiting()});
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
