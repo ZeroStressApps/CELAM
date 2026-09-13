a85a9b81a99324f951b7e1eb5fa34f1a453b707f
Lau <l.somacarrera@gmail.com>
2026-09-13 21:35:03 +0200

    Add files via upload
    CELAM-28-arbol-familia-miron-ortega

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE="celam-v24-retos-instant-save";
-const FILES=["./","./index.html","./style.css","./data.js","./app.js","./auth.js","./manifest.webmanifest","./assets/celam-logo.png"];
+const CACHE="celam-v28-arbol";
+const FILES=["./","./index.html","./style.css","./data.js","./app.js","./auth.js","./manifest.webmanifest","./assets/celam-logo.png","./assets/familia-miron-ortega.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request))));
