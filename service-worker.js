7dd4b448dc035a7ffb3c02eb4491545ddc5faa58
Lau <l.somacarrera@gmail.com>
2026-09-14 12:15:40 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,53 @@
-const CACHE="celam-v28-arbol";
-const FILES=["./","./index.html","./style.css","./data.js","./app.js","./auth.js","./manifest.webmanifest","./assets/celam-logo.png","./assets/familia-miron-ortega.png"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting())));
-self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request))));
+const CACHE = "celam-v29";
+
+const FILES = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./data.js",
+  "./app.js",
+  "./auth.js",
+  "./manifest.webmanifest",
+  "./assets/celam-logo.png",
+  "./assets/familia-miron-ortega.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then(cache => cache.addAll(FILES))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys =>
+        Promise.all(
+          keys
+            .filter(key => key !== CACHE)
+            .map(key => caches.delete(key))
+        )
+      )
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+
+        caches.open(CACHE).then(cache => {
+          cache.put(event.request, copy);
+        });
+
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
